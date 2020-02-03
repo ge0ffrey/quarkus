657f1c6b90ff24d284629c2d13596155f523e77f
Julian Cui <jucui@redhat.com>
2020-02-03 14:39:32 -0500

    Add illegal scan annotated classes test

---
+++ extensions/optaplanner/deployment/src/test/java/io/quarkus/optaplanner/OptaPlannerProcessorIllegalXMLTest.java
@@ -0,0 +1,25 @@
+package io.quarkus.optaplanner;
+
+import io.quarkus.test.QuarkusUnitTest;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+public class OptaPlannerProcessorIllegalXMLTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .setExpectedException(IllegalArgumentException.class)
+            .overrideConfigKey("quarkus.optaplanner.solver-config-xml", "io/quarkus/optaplanner/" +
+                    "illegalScanAnnotatedSolverConfig.xml")
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsResource("io/quarkus/optaplanner/illegalScanAnnotatedSolverConfig.xml"));
+
+    @Test
+    public void scanAnnotatedClasses() {
+        // Should not be called, deployment exception should happen first
+        Assertions.fail();
+    }
+}
